b738976a5b6db8141fc8196371cbe2c0709fc25d
shripada <shripada.hebbar@codecraft.co.in>
2023-08-07 11:37:06 +0530

    feat: implemented divide with minimum tests

--- math.js
+++ math.js
@@ -1,1 +1,12 @@
 // This module is going to  contain the add, subtract, multiply and divide functions
+function divide(a, b) {
+  return a / b;
+}
+
+// Test code which confirms divide works
+function testAdd() {
+  //Test case: Dividing 10 by 2 must be 5
+  const expected = 5;
+
+  assert(expected === divide(10, 5), 'dividing 10 by 2 must be 5'); // invariant
+}
