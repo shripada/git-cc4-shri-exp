3ba8acf842e328657bc581f85499c4ee37543df1
NIKHILS\nikhils <nikhil.s@codecraft.co.in>
2023-08-11 10:45:20 +0530

    feat: added fibonacci function

--- math.js
+++ math.js
@@ -49,3 +49,26 @@ function power(base, exponent) {
   }
   return product;
 }
+
+/**
+ * 
+ * @param {number} n count of fibonnaci numbers
+ * @returns array of n fibonacci numbers 
+ */
+function fibonacciSequence(n) {
+  if (n <= 0) {
+    return [];
+  } else if (n === 1) {
+    return [0];
+  } else if (n === 2) {
+    return [0, 1];
+  }
+
+  const sequence = [0, 1];
+  for (let i = 2; i < n; i++) {
+    const nextNumber = sequence[i - 1] + sequence[i - 2];
+    sequence.push(nextNumber);
+  }
+
+  return sequence;
+}
