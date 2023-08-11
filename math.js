8b920f365b74834b33fa2d1bf9ed77490e5988cd
NIKHILS\nikhils <nikhil.s@codecraft.co.in>
2023-08-11 11:05:21 +0530

    docs: added jsdoc for oddEven function

--- math.js
+++ math.js
@@ -79,7 +79,11 @@ function fibonacciSequence(n) {
   return sequence;
 }
 
-
+/**
+ * Funtion to check if number is even or odd
+ * @param {number} num 
+ * @returns 'even' if number is even else 'odd'
+ */
 function oddEven(num){
   if(num%2===0) return 'even'
   return 'odd';
