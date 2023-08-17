5d5192c36e5dea323e1b96ca61b8ae3cdbeefb98
shripada <shripada.hebbar@codecraft.co.in>
2023-08-17 10:17:56 +0530

    docs: documented trimString

--- strings.js
+++ strings.js
@@ -1,3 +1,8 @@
+/**
+ * Trims the string by removing white spaces in the beginning and the end
+ * @param {string} string
+ * @returns trimmed string
+ */
 function trimString(string) {
   const pattern = /^\s+|\s+$/g;
   return string.replace(pattern, '');
